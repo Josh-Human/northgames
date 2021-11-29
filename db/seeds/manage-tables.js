e86e6d5ffae1fb7449c1609f25972809d91e3cb8
Josh-Human <jhumanb1@gmail.com>
2021-11-29 11:59:24 +0000

    seed refactored to extract drop and create tables logic

---
+++ db/seeds/manage-tables.js
@@ -0,0 +1,58 @@
+const db = require("../connection.js");
+
+exports.dropTables = () => {
+  return db
+    .query(`DROP TABLE IF EXISTS comments;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS reviews;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS categories;`);
+    });
+};
+
+exports.createTables = () => {
+  return db
+    .query(
+      `CREATE TABLE categories (
+        slug VARCHAR(50) PRIMARY KEY,
+        description VARCHAR NOT NULL
+    );`
+    )
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+        username VARCHAR(50) PRIMARY KEY,
+        avatar_url TEXT NOT NULL,
+        name VARCHAR(50) NOT NULL
+      );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE reviews (
+          review_id SERIAL PRIMARY KEY,
+          title VARCHAR(100) NOT NULL,
+          review_body TEXT NOT NULL,
+          designer VARCHAR(100) NOT NULL,
+          review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+          votes INT DEFAULT 0,
+          category VARCHAR(50) REFERENCES categories(slug),
+          owner VARCHAR(50) REFERENCES users(username),
+          created_at DATE NOT NULL
+        );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          author VARCHAR(50) NOT NULL,
+          review_body TEXT NOT NULL,
+          designer VARCHAR(100) NOT NULL,
+          review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+          votes INT DEFAULT 0,
+          category VARCHAR(50) REFERENCES categories(slug),
+          owner VARCHAR(50) REFERENCES users(username),
+          created_at DATE NOT NULL
+        );`);
+    });
+};
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,8 @@
 const db = require("../connection.js");
+const { dropTables, createTables } = require("./manage-tables.js");
 
 //// if exists drop
+// TODO: Make table management functions.
 // TODO: Make drop async function.
 // TODO: Create categories.
 // TODO: Create user.
@@ -9,56 +11,9 @@ const db = require("../connection.js");
 
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  return db
-    .query(`DROP TABLE IF EXISTS comments;`)
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS reviews;`);
-    })
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users;`);
-    })
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS categories;`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE categories (
-        slug VARCHAR(50) PRIMARY KEY,
-        description VARCHAR NOT NULL
-      );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE users (
-      username VARCHAR(50) PRIMARY KEY,
-      avatar_url TEXT NOT NULL,
-      name VARCHAR(50) NOT NULL
-    );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE reviews (
-        review_id SERIAL PRIMARY KEY,
-        title VARCHAR(100) NOT NULL,
-        review_body TEXT NOT NULL,
-        designer VARCHAR(100) NOT NULL,
-        review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
-        votes INT DEFAULT 0,
-        category VARCHAR(50) REFERENCES categories(slug),
-        owner VARCHAR(50) REFERENCES users(username),
-        created_at DATE NOT NULL
-      );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE comments (
-        comment_id SERIAL PRIMARY KEY,
-        author VARCHAR(50) NOT NULL,
-        review_body TEXT NOT NULL,
-        designer VARCHAR(100) NOT NULL,
-        review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
-        votes INT DEFAULT 0,
-        category VARCHAR(50) REFERENCES categories(slug),
-        owner VARCHAR(50) REFERENCES users(username),
-        created_at DATE NOT NULL
-      );`);
-    });
+  return dropTables().then(() => {
+    return createTables;
+  });
 
   // 2. insert data
 };
